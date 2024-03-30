6e4fb0abb572625d1310fb91d080854681841fb8
MayurHattekar <hattekarmay@gmail.com>
2024-03-30 16:16:19 +0530

    first commit

---
+++ src/main/java/com/devops/javaDemoDevops/JavaDemoDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.javaDemoDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoDevopsApplication.class, args);
+	}
+
+}
